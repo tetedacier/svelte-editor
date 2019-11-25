f05a8bd3e1ea519532203fc1dc25156b5dc96592
tetedacier <fabien.jacq@gmail.com>
2019-11-25 16:05:20 +0100

    Tempory fix console height handling
    => This component should be renamed and its previous feature moved
    elsewhere

--- src/components/Console.svelte
+++ src/components/Console.svelte
@@ -1,21 +1,69 @@
 <script>
+import { createEventDispatcher, onMount, beforeUpdate, afterUpdate, tick } from 'svelte'
+import { fade, fly } from 'svelte/transition';
+
 import ConsoleLabel from './Console/label.svelte'
+
+$: consoleLabelHeight = 20
+
 export let grammar = 'text'
 export let path = ''
+export const getNumberFieldOnIfExist = (id, field) =>
+  document.getElementById(id)
+    ? document.getElementById(id)[field]
+    : 0
+
+afterUpdate(()=> {
+  console.warn(getNumberFieldOnIfExist('console','offsetHeight'))
+})
+beforeUpdate(()=> {
+  console.warn(getNumberFieldOnIfExist('console','offsetHeight'))
+})
+
 $: showConsole = false
+$: consoleContentHeight = 0;
+const dispatch = createEventDispatcher();
+
 const updateConsoleDisplay = async (event) => {
+  const consoleContent=document.getElementById('console-content')
+  if(consoleContent) {
+    console.warn(consoleContent);
+  }
   showConsole = event.detail.checked
 }
+const resize = (source) => (event) => {
+  tick()
+  tick()
+  tick()
+  tick()
+  tick()
+  console.warn({ source, showConsole, event, heigth: document.getElementById('console').offsetHeight })
+  dispatch('resize', {
+    source,
+    height: (source ==='out')?document.getElementById('console-content').offsetHeight:0
+  })
+}
+onMount(() => {
+  // consoleContentHeight = document.getElementById('console-content').offsetHeight;
+  // console.warn(consoleContentHeight)
+})
+
 </script>
-<form>
+<style>
+fieldset {
+  background-color: azure;
+}
+
+</style>
+<form id="console">
+  <ConsoleLabel id="console-label" on:update={updateConsoleDisplay} showConsole={showConsole} bind:height={consoleLabelHeight}/>
   {#if showConsole}
-    <ConsoleLabel on:update={updateConsoleDisplay} showConsole={showConsole}/>
-    <fieldset>
+    <fieldset
+      id="console-content"
+    >
       <legend>
       </legend>
       <code class={grammar}>{path}</code>
     </fieldset>
-  {:else}
-    <ConsoleLabel on:update={updateConsoleDisplay}  showConsole={showConsole} />
   {/if}
 </form>
--- src/components/Console/label.svelte
+++ src/components/Console/label.svelte
@@ -1,16 +1,26 @@
 <script>
-import { createEventDispatcher } from 'svelte'
+import { onMount, createEventDispatcher } from 'svelte'
 const dispatch = createEventDispatcher();
 export let showConsole = false
+export let id = "id"
+export let height = 20;
+onMount(()=>{
+  height = document.getElementById(id).offsetHeight;
+})
 $: consoleShown = showConsole
 const toggleConsoleMode=(event)=>{
-  console.warn(consoleShown)
   dispatch('update',{
     checked: consoleShown
   })
 }
 </script>
-<label>
-  <input on:change={toggleConsoleMode} type="checkbox" bind:checked={consoleShown} />
+<style>
+label{
+  display: block;
+  background-color: aqua;
+}
+</style>
+<label id={id} for={`console-input-checkbox-${id}`}>
+  <input id={`console-input-checkbox-${id}`} on:change={toggleConsoleMode} type="checkbox" bind:checked={consoleShown} />
   Console
 </label>
