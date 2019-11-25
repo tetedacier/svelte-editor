c69d5ec1fa38b199fa6a5212f1644043cbe06517
tetedacier <fabien.jacq@gmail.com>
2019-11-25 14:19:41 +0100

    Create ace bundle script

--- bin/ace-bundle.sh
+++ bin/ace-bundle.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+# rebuild ace if needed
+if [[ ! -f public/ace/src-min/ace.js ]]; then
+  node ace/Makefile.dryice.js -m -target public/ace
+fi
+
 build_ace () {
-  for path in public/ace/src-min/{ace.js,theme-monokai.js,{mode-,snippets/}{css,text,javascript,json,markdown,yaml,twig,typescript,html}.js}; do
+  for path in public/ace/src-min/{ace.js,theme-monokai.js,{mode-,snippets/}{css,text,javascript,xml,json,markdown,yaml,twig,typescript,html}.js}; do
     cat $path
   done
 }
 build_ace > public/ace-bundle.js
-# build_ace
